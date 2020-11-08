4fec3601a6d0cb7effce8a56a83d36c42a6ebfd5
UO264254 <UO264254@uniovi.es>
2020-11-08 16:05:55 +0100

    practica 3

---
+++ PRACTICA-3/Ejercicio-01/Cabecera.js
@@ -0,0 +1,10 @@
+// Cabecera.js
+// Informacion de asignatura
+var asignatura = new Object();
+asignatura.nombre = "Software y Estándares para la Web";
+asignatura.titulacion = "Grado en Ingeniería Informática del Software";
+asignatura.centro = "Escuela de Ingeniería Informática";
+asignatura.universidad = "Universidad de Oviedo";
+asignatura.curso = "2020-21";
+asignatura.estudiante = "Alejo Brandy García-Rovés";
+asignatura.email = "uo264254@uniovi.es";
---
+++ PRACTICA-3/Ejercicio-01/Parrafos.js
@@ -0,0 +1,10 @@
+/*Parrafos.js*/
+document.write("<p>Curso actual: ");
+document.write(asignatura.curso);
+document.write("</p>");
+document.write("<p>Estudiante: ");
+document.write(asignatura.estudiante);
+document.write("</p>");
+document.write("<p>e-mail: ");
+document.write(asignatura.email);
+document.write("</p>");
